782f05a053ae86168f0fdd6753e345356cf1ae5a
DiuMasum <masum35-577@gmail.com>
2024-06-02 00:33:25 +0600

    second

--- routes/web.php
+++ routes/web.php
@@ -21,9 +21,20 @@ Route::get('/', function () {
     return view('welcome');
 });
 
+Route::get('/dashboard', function () {
+    return view('dashboard');
+})->middleware(['auth', 'verified'])->name('dashboard');
+
+Route::middleware('auth')->group(function () {
+    Route::get('/profile', [ProfileController::class, 'edit'])->name('profile.edit');
+    Route::patch('/profile', [ProfileController::class, 'update'])->name('profile.update');
+    Route::delete('/profile', [ProfileController::class, 'destroy'])->name('profile.destroy');
+});
+
+require __DIR__.'/auth.php';
 
 
-Route::middleware([])->group(function () {
+Route::middleware('auth')->group(function () {
     Route::controller(DashboardController::class)->group(function () {
         Route::get('/admin/dashboard', 'index')->name('admindashboard');
     });
@@ -47,4 +58,3 @@ Route::middleware([])->group(function () {
     });
 
 });
-
